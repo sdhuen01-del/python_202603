661408e0a2f9de101b08052556c161754ab9ec0c
sdhuen01-del <sdhuen01@gmail.com>
2026-03-10 14:47:15 +0900

    20260310_module_패키지

---
+++ game/__init__.py
@@ -0,0 +1,10 @@
+# game.__init__.py
+
+from .graphic.render import render_test
+
+VERSION = 3.5
+
+def print_version_info():
+    print(f'The version of this gmae is {VERSION}')
+
+print("Initalizing game...")
